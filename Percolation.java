41e4ef148455715c4ae047aec6ded785ac12a682
Andrew James Zures <andrew.zures@gmail.com>
2013-02-24 15:07:58 -0600

    All Tests Passed

--- Percolation.java
+++ Percolation.java
@@ -2,23 +2,22 @@
 
 public class Percolation {
 
-  private Boolean[][] grid;
+  private byte[][] grid;
   private WeightedQuickUnionUF unionArray;
   private WeightedQuickUnionUF fullArray;
   private int gridWidth;
   private int sinkNum;
-
   
   public Percolation(int N) {
 	unionArray = new WeightedQuickUnionUF(N*N+2);
 	fullArray = new WeightedQuickUnionUF(N*N+2);
-    grid = new Boolean[N][N];
+    grid = new byte[N][N];
     gridWidth = N;
     sinkNum = gridWidth*gridWidth+1;
 
     for (int i = 0; i < N; i++) {
     		for (int j = 0; j < N; j++) {
-    			grid[i][j] = false;
+    			grid[i][j] = 0;
     		}
     }
   }
@@ -28,22 +27,22 @@ public class Percolation {
 	  if (i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
 	  if (j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
 	  if (!isOpen(i, j)) {
-		  grid[i-1][j-1] = true;
+		  grid[i-1][j-1] = 1;
 		  checkForConnections(i, j);
 	  }
   }
 		  
   public boolean isOpen(int i, int j) {
-	 if (i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
-	 if (j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
+	  if (i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
+	  if (j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
   
-    return grid[i-1][j-1] == true;
+    return (grid[i-1][j-1] == 1);
 
   }
   
   public boolean isFull(int i, int j) {
-	 if (i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
-	 if (j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
+	  if (i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
+	  if (j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
 	 
 	  return fullArray.connected(0, getOneDArrayIndex(i, j));
   }
@@ -58,24 +57,24 @@ public class Percolation {
   private void checkForConnections(int i, int j) {
 	  
 	  if (i > 1) {	
-		  checkUpSite(i,j);
+		  checkUpSite(i, j);
 		  }
 	  else {
 		  unionArray.union(getOneDArrayIndex(i, j), 0);
 		  fullArray.union(getOneDArrayIndex(i, j), 0);
 	  }
 	  			
-	  if (i < gridWidth) {checkDownSite(i, j); }
-	  else {unionArray.union(getOneDArrayIndex(i, j), sinkNum); }
+	  if (i < gridWidth) { checkDownSite(i, j); }
+	  else { unionArray.union(getOneDArrayIndex(i, j), sinkNum); }
 	  
-	  if (j > 1) {checkLeftSite(i,j); }
+	  if (j > 1) { checkLeftSite(i, j); }
 	  
-	  if (j < gridWidth) {checkRightSite(i, j); }
+	  if (j < gridWidth) { checkRightSite(i, j); }
   }
 		  
-  private void checkUpSite(int i, int j){
+  private void checkUpSite(int i, int j) {
 	  
-	if(isOpen(i-1, j)) {
+	if (isOpen(i-1, j)) {
 		unionArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i-1, j));
 		fullArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i-1, j));
 	}
@@ -92,12 +91,12 @@ public class Percolation {
   private void checkLeftSite(int i, int j) {
 
 	if (isOpen(i, j-1)) {
-		unionArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i, j-1));
-		fullArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i, j-1));	
+	  unionArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i, j-1));
+	  fullArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i, j-1));	
 	}
   }
 
-  private void checkRightSite(int i ,int j) {
+  private void checkRightSite(int i, int j) {
 	  
 	if (isOpen(i, j+1)) {
 		unionArray.union(getOneDArrayIndex(i, j), getOneDArrayIndex(i, j+1));
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,59 +1,63 @@
-
 public class PercolationStats {
 	private double[] results;
 	private int t;
 
-   public PercolationStats(int N, int T) {
-	   if (N <= 0 || T <= 0) { throw new IllegalArgumentException(); }
-	   t = T;
-	   results = new double[T];
-	   
-	   for (int i = 0; i < T; i++) {
-		   Percolation p = new Percolation(N);
-		   testPercolation(p, N, i);
-	   }
-   }
-	   
-   private void testPercolation(Percolation p, int N, int i) {
+	public PercolationStats(int N, int T) {
+		if (N <= 0 || T <= 0) {
+			throw new IllegalArgumentException();
+		}
+		t = T;
+		results = new double[T];
 
-	   int count = 0;
-	   while (!p.percolates()) {
-		 int randomI = StdRandom.uniform(N)+1;	  
-		 int randomJ = StdRandom.uniform(N)+1;
-		  //System.out.printf("Random: %d %d \n", randomI, randomJ);
-		 if (!p.isOpen(randomI, randomJ)) {
-			 p.open(randomI, randomJ);
-			 count++;
-		 }
-	  }
-	   results[i] = (double) count / (N*N); 
-   }
-	   
- // perform T independent computational experiments on an N-by-N grid
-   public double mean() {
-	   return StdStats.mean(results);
-   }
-   public double stddev() {
-	   return StdStats.stddev(results);
-   }
-   public double confidenceLo() {
-	   return StdStats.mean(results) 
-			   - (1.96 * StdStats.stddev(results)) / Math.sqrt(t);
-   }
-   public double confidenceHi() {
-	   return StdStats.mean(results) 
-			   + (1.96 * StdStats.stddev(results)) / Math.sqrt(t);
-   }
-   public static void main(String[] args) {
-	   
-	   int nArg = Integer.parseInt(args[0]);
-	   int tArg = Integer.parseInt(args[1]);
-	   
-	   PercolationStats stats = new PercolationStats(nArg, tArg);
-	   StdOut.print(stats.mean());
-	   StdOut.print(stats.stddev());
-	   StdOut.printf("%f, %f", stats.confidenceLo(), stats.confidenceHi());
-   }
-	
-	
+		for (int i = 0; i < T; i++) {
+			Percolation p = new Percolation(N);
+			testPercolation(p, N, i);
+		}
+	}
+
+	private void testPercolation(Percolation p, int N, int i) {
+
+		int count = 0;
+		while (!p.percolates()) {
+			int randomI = StdRandom.uniform(N) + 1;
+			int randomJ = StdRandom.uniform(N) + 1;
+			// System.out.printf("Random: %d %d \n", randomI, randomJ);
+			if (!p.isOpen(randomI, randomJ)) {
+				p.open(randomI, randomJ);
+				count++;
+			}
+		}
+		results[i] = (double) count / (N * N);
+	}
+
+	// perform T independent computational experiments on an N-by-N grid
+	public double mean() {
+		return StdStats.mean(results);
+	}
+
+	public double stddev() {
+		return StdStats.stddev(results);
+	}
+
+	public double confidenceLo() {
+		return StdStats.mean(results) - (1.96 * StdStats.stddev(results))
+				/ Math.sqrt(t);
+	}
+
+	public double confidenceHi() {
+		return StdStats.mean(results) + (1.96 * StdStats.stddev(results))
+				/ Math.sqrt(t);
+	}
+
+	public static void main(String[] args) {
+
+		int nArg = Integer.parseInt(args[0]);
+		int tArg = Integer.parseInt(args[1]);
+
+		PercolationStats stats = new PercolationStats(nArg, tArg);
+		StdOut.print(stats.mean());
+		StdOut.print(stats.stddev());
+		StdOut.printf("%f, %f", stats.confidenceLo(), stats.confidenceHi());
+	}
+
 }
