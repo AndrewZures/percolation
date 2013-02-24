0d9c2f68a859caa0ef965c8a459079316833b4cc
Andrew James Zures <andrew.zures@gmail.com>
2013-02-24 14:12:34 -0600

    minor changes

--- Percolation.java
+++ Percolation.java
@@ -2,25 +2,24 @@
 
 public class Percolation {
 	
-  private int[][] grid;
+  private int[] grid;
   private WeightedQuickUnionUF unionArray;
   private WeightedQuickUnionUF fullArray;
   private int gridWidth;
   private int sinkNum;
+  private int index;
 
   
   public Percolation (int N) {
 	unionArray = new WeightedQuickUnionUF(N*N+2);
 	fullArray = new WeightedQuickUnionUF(N*N+2);
-    grid = new int[N][N];
+    grid = new int[N*N];
     gridWidth = N;
     sinkNum = gridWidth*gridWidth+1;
 
-    for(int i = 0; i < N; i++) {
-      for(int j = 0; j < N; j++) {
-        grid[i][j] = 0;
+    for(int k = 0; k < N*N; k++) {
+        grid[k] = 0;
       }
-    }
   }
 
   
@@ -28,7 +27,7 @@ public class Percolation {
 	  if(i < 1 || i > gridWidth) throw new IndexOutOfBoundsException();
 	  if(j < 1 || j > gridWidth) throw new IndexOutOfBoundsException();
 	  if(!isOpen(i,j)){
-		  grid[i-1][j-1] = 1;
+		  grid[(i-1)*gridWidth + j-1] = 1;
 		  checkForConnections(i, j);
 	  }
   }
@@ -56,11 +55,13 @@ public class Percolation {
   }
   
   private void checkForConnections(int i, int j){
-	  if(i > 1){	checkUpSite(i,j);}
+	  
+	  if(i > 1){	
+		  checkUpSite(i,j);
+		  }
 	  else {
-		  int index = getOneDArrayIndex(i,j);
-		  unionArray.union(index,0);
-		  fullArray.union(index,0);
+		  unionArray.union(getOneDArrayIndex(i,j),0);
+		  fullArray.union(getOneDArrayIndex(i,j),0);
 		  }
 	  			
 	  if(i < gridWidth){checkDownSite(i,j);}
@@ -69,40 +70,37 @@ public class Percolation {
 	  if(j > 1){checkLeftSite(i,j);}
 	  
 	  if(j < gridWidth){checkRightSite(i,j);}
-
   }
 		  
- 
   private void checkUpSite(int i, int j){
+	  
 	if(isOpen(i-1, j)) {
-		int index = getOneDArrayIndex(i,j);
-		unionArray.union(index, getOneDArrayIndex(i-1, j));
-		fullArray.union(index, getOneDArrayIndex(i-1, j));
+		unionArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i-1, j));
+		fullArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i-1, j));
 	}
   }
 
   private void checkDownSite(int i, int j){
+	  
 	if(isOpen(i+1, j)) {
-		int index = getOneDArrayIndex(i,j);
-		unionArray.union(index, getOneDArrayIndex(i+1, j));
-		fullArray.union(index, getOneDArrayIndex(i+1, j));
+		unionArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i+1, j));
+		fullArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i+1, j));
 	}
   }
 
   private void checkLeftSite(int i, int j){
-	
+
 	if(isOpen(i, j-1)) {
-		int index = getOneDArrayIndex(i,j);
-		unionArray.union(index, getOneDArrayIndex(i, j-1));
-		fullArray.union(index, getOneDArrayIndex(i, j-1));	
+		unionArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i, j-1));
+		fullArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i, j-1));	
 	}
   }
 
   private void checkRightSite(int i ,int j){
+	  
 	if(isOpen(i, j+1)) {
-		int index = getOneDArrayIndex(i,j);
-		unionArray.union(index, getOneDArrayIndex(i, j+1));
-		fullArray.union(index, getOneDArrayIndex(i, j+1));
+		unionArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i, j+1));
+		fullArray.union(getOneDArrayIndex(i,j), getOneDArrayIndex(i, j+1));
 	}
   }
   
